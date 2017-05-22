1fcb66f932f4bfe0e5bafd5044176018c869a6bf
Lars Cornelissen <larspkcornelissen@gmail.com>
2017-05-22 10:00:57 +0200

    getting exercises

--- src/app/crud/sport/exercise/exercise.service.ts
+++ src/app/crud/sport/exercise/exercise.service.ts
@@ -1,42 +1,65 @@
-import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/operator/map';
-import {Http, Response}          from '@angular/http';
+import {Injectable} from "@angular/core";
+import "rxjs/add/operator/catch";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/toPromise";
+import {Headers, Http} from "@angular/http";
 import {Exercise} from "./exercise";
-
 
 @Injectable()
 export class ExerciseService {
 
-  private exerciseUrl = 'localhost:8080/exercises';  // URL to web API
+  private exercisesUrl = 'http://145.28.225.69:8080/api/exercises/';  // URL to web API
+  private headers = new Headers();
 
 
   constructor(private http: Http) {
   }
 
-  getExercises(): Observable<Exercise[]> {
-    return this.http.get(this.exerciseUrl)
-      .map(this.extractData)
+  getExercises(): Promise<Exercise[]> {
+    this.headers.append('Authorization', 'Basic ' + btoa('TSSE:welkom123'));
+    this.headers.append('getExercises', 'text/plain');
+    return this.http.get(this.exercisesUrl
+      , {headers : this.headers}
+    ).toPromise()
+      .then(response => response.json().data as Exercise[])
       .catch(this.handleError);
   }
 
-  private extractData(res: Response) {
-    let body = res.json();
-    return body.data || {};
+  getExercise(id: number): Promise<Exercise> {
+    const url = `${this.exercisesUrl}/${id}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(response => response.json().data as Exercise)
+      .catch(this.handleError);
   }
 
-  private handleError(error: Response | any) {
-    // In a real world app, you might use a remote logging infrastructure
-    let errMsg: string;
-    if (error instanceof Response) {
-      const body = error.json() || '';
-      const err = body.error || JSON.stringify(body);
-      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-    } else {
-      errMsg = error.message ? error.message : error.toString();
-    }
-    console.error(errMsg);
-    return Observable.throw(errMsg);
+  delete(id: number): Promise<void> {
+    const url = `${this.exercisesUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError);
+  }
+
+  create(name: string): Promise<Exercise> {
+    return this.http
+      .post(this.exercisesUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().data as Exercise)
+      .catch(this.handleError);
+  }
+
+  update(exercise: Exercise): Promise<Exercise> {
+    const url = `${this.exercisesUrl}/${exercise.id}`;
+    return this.http
+      .put(url, JSON.stringify(exercise), {headers: this.headers})
+      .toPromise()
+      .then(() => exercise)
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
   }
 }
